b7afe8b25df3b15a60fdc848ee769185693ff43b
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-05 15:27:20 +0530

    Time: 18 ms (91.70%) | Memory: 105.3 MB (64.98%) - LeetSync

--- 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.cpp
+++ 2419-subarray-with-elements-greater-than-varying-threshold/subarray-with-elements-greater-than-varying-threshold.cpp
@@ -2,31 +2,39 @@ class Solution {
 public:
     int validSubarraySize(vector<int>& nums, int threshold) {
         int n = nums.size();
-        vector<int> left(n), right(n);
-
-        // Previous smaller element index
+        vector<int> left(n, -1);
+        vector<int> right(n, n);
         stack<int> st;
+        
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
-            left[i] = st.empty() ? -1 : st.top();
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                left[i] = st.top();
+            }
             st.push(i);
         }
-
-        // Next smaller element index
-        while (!st.empty()) st.pop();
+        
+        st = stack<int>();
+        
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && nums[st.top()] >= nums[i]) st.pop();
-            right[i] = st.empty() ? n : st.top();
+            while (!st.empty() && nums[st.top()] >= nums[i]) {
+                st.pop();
+            }
+            if (!st.empty()) {
+                right[i] = st.top();
+            }
             st.push(i);
         }
-
-        // Check each nums[i] as minimum in its span
+        
         for (int i = 0; i < n; i++) {
-            int span = right[i] - left[i] - 1;  // max length where nums[i] is min
-            // Minimal length required so that nums[i] * k > threshold
-            int k_min = threshold / nums[i] + 1;
-            if (k_min <= span) return k_min;   // return the valid subarray length
+            int k = right[i] - left[i] - 1;
+            if (nums[i] > threshold / k) {
+                return k;
+            }
         }
+        
         return -1;
     }
 };
