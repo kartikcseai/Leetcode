3763ce6ead3776c03f4ab7a091e490fa1d859327
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-14 21:14:57 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (49.40%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> res;
+        if (matrix.empty()) return res;
+
+        int top = 0;
+        int bottom = matrix.size() - 1;
+        int left = 0;
+        int right = matrix[0].size() - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse top row
+            for (int i = left; i <= right; ++i)
+                res.push_back(matrix[top][i]);
+            top++;
+
+            // Traverse right column
+            for (int i = top; i <= bottom; ++i)
+                res.push_back(matrix[i][right]);
+            right--;
+
+            // Traverse bottom row (if still valid)
+            if (top <= bottom) {
+                for (int i = right; i >= left; --i)
+                    res.push_back(matrix[bottom][i]);
+                bottom--;
+            }
+
+            // Traverse left column (if still valid)
+            if (left <= right) {
+                for (int i = bottom; i >= top; --i)
+                    res.push_back(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return res;
+    }
+};
