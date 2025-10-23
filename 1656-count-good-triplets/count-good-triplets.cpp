fc8f26ed0d0d3457bc79d305241425f6a6390c2c
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-23 07:13:06 +0530

    Time: 10 ms (34.32%) | Memory: 10.9 MB (93.04%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.cpp
+++ 1656-count-good-triplets/count-good-triplets.cpp
@@ -3,7 +3,6 @@ public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
         int n = arr.size();
         int count = 0;
-
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (abs(arr[i] - arr[j]) > a) continue;
@@ -14,7 +13,6 @@ public:
                 }
             }
         }
-
         return count;
     }
 };
