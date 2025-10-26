c518181ed98f4b0b5fcdc44cc21723c905bc062f
Karitk Gupta <csai23031@glbitm.ac.in>
2025-10-26 15:12:02 +0530

    Time: 26 ms (56.50%) | Memory: 59.4 MB (91.03%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.cpp
@@ -0,0 +1,30 @@
+class MyHashMap {
+public:
+    map<int, int> m;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    int get(int key) {
+        if (m.find(key) != m.end()) {
+            return m[key];  // Return value if key exists
+        }
+        return -1;  // Return -1 if key does not exist
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
