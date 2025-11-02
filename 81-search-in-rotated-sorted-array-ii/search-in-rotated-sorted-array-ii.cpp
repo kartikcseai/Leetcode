04261a614dccca8ab5db412727f7bd9dad8bd133
Karitk Gupta <csai23031@glbitm.ac.in>
2025-11-02 08:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (55.26%) - LeetSync

---
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        bool flag=false;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target) {
+                flag=true;
+                break;
+            }
+        }
+        if(flag==true) return true;
+        else return false;
+    }
+};
