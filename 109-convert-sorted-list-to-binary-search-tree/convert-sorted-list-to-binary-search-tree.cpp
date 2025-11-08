c0179c919b959e85b34b2241cb9d2aeee4c2f51d
Karitk Gupta <csai23031@glbitm.ac.in>
2025-11-08 22:44:26 +0530

    Time: 7 ms (28.44%) | Memory: 33.2 MB (28.48%) - LeetSync

--- 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -1,42 +1,29 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* helper(vector<int>& nums, int lo, int hi){
-        if(lo>hi) return NULL;
-        int mid=lo+(hi-lo)/2;
-        TreeNode* root= new TreeNode(nums[mid]);
-        root->left=helper(nums, lo, mid-1);
-        root->right=helper(nums, mid+1, hi);
+    int getLength(ListNode* head) {
+        int len = 0;
+        while (head) {
+            len++;
+            head = head->next;
+        }
+        return len;
+    }
+    TreeNode* buildBST(ListNode*& head, int lo, int hi) {
+        if (lo > hi) return nullptr;
+
+        int mid = lo + (hi - lo) / 2;
+
+        TreeNode* left = buildBST(head, lo, mid - 1);
+        TreeNode* root = new TreeNode(head->val);
+        head = head->next;
+        TreeNode* right = buildBST(head, mid + 1, hi);
+
+        root->left = left;
+        root->right = right;
         return root;
     }
     TreeNode* sortedListToBST(ListNode* head) {
-        vector<int> nums;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            nums.push_back(temp->val);
-            temp=temp->next;
-        }
-        int n=nums.size();
-        return helper(nums, 0, n-1);
+        int n = getLength(head);
+        return buildBST(head, 0, n - 1);
     }
 };
