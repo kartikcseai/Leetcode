7b5d1996a921a9093a3192d9fb87ca368ddde69f
Karitk Gupta <csai23031@glbitm.ac.in>
2025-11-30 21:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (81.30%) - LeetSync

---
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int smallestRepunitDivByK(int K) {
+        // If K has factors 2 or 5, no repunit can be divisible by K
+        if (K % 2 == 0 || K % 5 == 0) return -1;
+
+        int remainder = 0;
+        for (int length = 1; length <= K; ++length) {
+            remainder = (remainder * 10 + 1) % K;
+            if (remainder == 0) return length;
+        }
+
+        return -1; // Should not happen for valid K
+    }
+};
