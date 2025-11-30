66c4a8b2cf25ad3d1ca6cc13ab4e55ddc441e7e1
Karitk Gupta <csai23031@glbitm.ac.in>
2025-11-30 21:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (88.84%) - LeetSync

---
+++ 331-verify-preorder-serialization-of-a-binary-tree/verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        int slot=1, n=preorder.size();
+        for(int i=0;i<n;i++){
+            if(preorder[i]==',') continue; // skip comma
+            slot--;
+            if(slot<0) return false;
+            if(preorder[i]!='#'){
+                slot+=2;
+                while(i<n && preorder[i]!=',') i++;
+            }
+        }
+        return slot==0;
+    }
+};
