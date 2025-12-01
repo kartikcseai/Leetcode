7918f715db56fca7d3f1d0d9c6506a1857fb3c80
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-01 22:02:46 +0530

    Time: 6 ms (97.81%) | Memory: 19.1 MB (54.61%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,34 +1,23 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(root==NULL) return NULL;
-        Node* leftmost=root; // left most node of current level
-        while(leftmost->left){
-            Node* head=leftmost;
-            while(head!=NULL){
-                head->left->next=head->right;
-                if(head->next!=NULL) head->right->next=head->next->left;
-                head=head->next;
+        if(root==NULL || root->left==NULL) return root;
+        queue<Node*> q;
+        q.push(root);
+        q.push(NULL);
+        Node* prev=NULL;
+        while(q.size()>0){
+            Node* curr=q.front(); q.pop();
+            if(curr==NULL){
+                if(q.size()==0) break;
+                q.push(NULL);
             }
-            leftmost=leftmost->left;
+            else {
+                if(curr->left!=NULL) q.push(curr->left);
+                if(curr->right!=NULL) q.push(curr->right);
+                if(prev!=NULL) prev->next=curr;
+            }
+            prev=curr;
         }
         return root;
     }
