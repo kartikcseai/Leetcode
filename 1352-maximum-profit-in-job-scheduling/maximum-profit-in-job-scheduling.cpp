347443291cc9e6aee86721acecb206b2a42999e7
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-09 19:04:05 +0530

    Time: 70 ms (59.02%) | Memory: 92 MB (49.07%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,21 +1,35 @@
 class Solution {
 public:
+    vector<int> memo;
+    int solve(int i, vector<vector<int>>& jobs) {
+        if (i==jobs.size()) return 0;
+        if (memo[i]!=-1) return memo[i];
+        int start=jobs[i][0];
+        int end=jobs[i][1];
+        int profit=jobs[i][2];
+        // binary search for next non-conflicting job
+        int l=i+1, r=jobs.size()-1, nxt=jobs.size();
+        while (l<=r) {
+            int mid=(l+r)/2;
+            if(jobs[mid][0]>=end) {
+                nxt=mid;
+                r=mid-1;
+            }
+            else l=mid+1;
+        }
+        int take=profit+solve(nxt, jobs);
+        int skip=solve(i + 1, jobs);
+
+        return memo[i]=max(take, skip);
+    }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n=startTime.size();
-        vector<array<int,3>> jobs(n);
-        for(int i=0;i<n;i++) jobs[i]={endTime[i],startTime[i],profit[i]};
-        sort(jobs.begin(),jobs.end()); // sort by endTime
-        vector<int> dp(n);
-        vector<int> ends(n); // For binary search, use a separate array of end times
-        for(int i=0;i<n;i++) ends[i]=jobs[i][0];
-        for(int i=0;i<n;i++){
-            int e=jobs[i][0], s=jobs[i][1], p=jobs[i][2];
-            // find non-conflicting jobs
-            int j=upper_bound(ends.begin(), ends.end(),s)-ends.begin()-1;
-            int includeProfit=p+(j>=0?dp[j]:0);
-            int excludeProfit=(i>0?dp[i-1]:0);
-            dp[i]=max(includeProfit,excludeProfit);
-        }
-        return dp[n-1];
+        vector<vector<int>> jobs(n);
+        for (int i=0;i<n;i++)
+            jobs[i]={startTime[i], endTime[i], profit[i]};
+
+        sort(jobs.begin(), jobs.end()); // sort by start time
+        memo.assign(n, -1);
+        return solve(0, jobs);
     }
 };
