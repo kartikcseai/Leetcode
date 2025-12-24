fb8316a26544bd3427bd2f27d3b72fa8c036c268
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-24 11:21:37 +0530

    Time: 23 ms (82.99%) | Memory: 129 MB (68.71%) - LeetSync

--- 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
+++ 3382-find-the-number-of-subarrays-where-boundary-elements-are-maximum/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -3,19 +3,12 @@ public:
     long long numberOfSubarrays(vector<int>& nums) {
         long long ans = 0;
         vector<pair<int, long long>> st; // {value, count}
-
         for (int x : nums) {
-            // Remove smaller elements
-            while (!st.empty() && st.back().first < x) {
-                st.pop_back();
-            }
-
+            while (!st.empty() && st.back().first < x)st.pop_back();
             if (!st.empty() && st.back().first == x) {
-                // Extend existing group
                 st.back().second++;
                 ans += st.back().second;
             } else {
-                // New value
                 st.push_back({x, 1});
                 ans += 1;
             }
