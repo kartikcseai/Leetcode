362914006d934883eafe16b14cc8cce5c5e4ada1
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-24 08:22:22 +0530

    Time: 0 ms (100.00%) | Memory: 33.1 MB (45.84%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
-        int count_apples=0;
-        for(int i=0;i<apple.size();i++) count_apples+=apple[i];
-        sort(capacity.rbegin(),capacity.rend());
-        int check_capacity=0, count=0;
-        for(int i=0;i<capacity.size();i++){
-            if(check_capacity<count_apples){
-                check_capacity+=capacity[i];
-                count++;
-            }
+        int sum = accumulate(apple.begin(), apple.end(), 0);
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        int need = 0;
+        while (sum > 0) {
+            sum -= capacity[need];
+            need += 1;
         }
-        return count;
+        return need;
     }
 };
