88642a03746d555b1f9015cfe2b9edb53efd1f3c
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-28 15:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (53.29%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -2,12 +2,10 @@ class Solution {
 public:
     vector<string> letterCombinations(string digits) {
         if (digits.empty()) return {};
-
         vector<string> mapping = {
             "",    "",    "abc", "def", "ghi", "jkl",
             "mno", "pqrs", "tuv", "wxyz"
         };
-
         vector<string> result;
         string current;
         backtrack(digits, 0, mapping, current, result);
@@ -20,7 +18,6 @@ public:
             result.push_back(current);
             return;
         }
-
         string letters = mapping[digits[index] - '0'];
         for (char c : letters) {
             current.push_back(c);
