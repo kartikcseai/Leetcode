ffaf50a339138e79aa29ce0d3e7ef4c9531637ae
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-28 15:44:33 +0530

    Time: 4 ms (88.12%) | Memory: 22.9 MB (43.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    // Helper function to calculate total hours needed at a given eating speed k
     bool canFinish(const vector<int>& piles, int k, int h) {
         long long hours = 0;
-        for (int pile : piles) {
-            // Using (pile + k - 1) / k instead of ceil(pile / k) for integer math
-            hours += (pile + k - 1) / k;
-        }
+        for (int pile : piles) hours += (pile + k - 1) / k;
         return hours <= h;
     }
-
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1;  // Minimum possible speed
-        int right = *max_element(piles.begin(), piles.end()); // Maximum possible speed (largest pile)
+        int left = 1;  
+        int right = *max_element(piles.begin(), piles.end()); 
         int result = right;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
-
             if (canFinish(piles, mid, h)) {
-                result = mid;     // mid works, try smaller speed
+                result = mid;   
                 right = mid - 1;
-            } else {
-                left = mid + 1;   // mid too slow, increase speed
-            }
+            } 
+            else left = mid + 1; 
         }
 
         return result;
