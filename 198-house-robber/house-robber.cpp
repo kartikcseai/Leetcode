970c976c1986ecd4fdc2d787a94e5ebda89e4270
Karitk Gupta <csai23031@glbitm.ac.in>
2025-12-29 16:13:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (69.59%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<int> dp;
     int profit(vector<int>& arr, int i){
-        if(i==arr.size()-1) return arr[i]; // single house
-        if(i==arr.size()-2) return max(arr[i], arr[i+1]); // 2 houses
+        if(i==arr.size()-1) return arr[i]; 
+        if(i==arr.size()-2) return max(arr[i], arr[i+1]); 
         if(dp[i]!=-1) return dp[i];
         return dp[i]=max(arr[i]+profit(arr, i+2), profit(arr, i+1));
     }
     int rob(vector<int>& nums) {
         dp.clear();
-        dp.resize(105,-1); // constraints mein limit se zadya ka size declare kar do -> CP 
+        dp.resize(105,-1); 
         return profit(nums, 0);
     }
 };
