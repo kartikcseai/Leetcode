89b78c19780925413936c78d8d2e3eaa6a4fcc38
Karitk Gupta <csai23031@glbitm.ac.in>
2026-01-03 09:34:14 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (38.07%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode dummy(0);
-        dummy.next=head;
-        ListNode* prev=&dummy;
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* prev=dummy;
         while(head!=NULL){
             if(head->next!=NULL && head->val==head->next->val){
                 int dupVal=head->val;
@@ -13,6 +13,6 @@ public:
                 prev=head;
                 head=head->next;}
         }
-        return dummy.next;
+        return dummy->next;
     }
 };
