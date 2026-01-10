cbfcaf94f3881f875fe498d89d017708b7b2def2
Karitk Gupta <csai23031@glbitm.ac.in>
2026-01-10 08:00:48 +0530

    Time: 0 ms (100.00%) | Memory: 29.7 MB (27.24%) - LeetSync

---
+++ 495-teemo-attacking/teemo-attacking.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans=0;
+        for(int i=0;i<timeSeries.size()-1;i++){
+            if(timeSeries[i+1]-timeSeries[i]>=duration) ans+=duration;
+            else ans+=timeSeries[i+1]-timeSeries[i];
+        }
+        ans+=duration;
+        return ans;
+    }
+};
