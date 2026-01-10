f90a98b2dc18be99ee4f53cc6ff81723e3daf469
Karitk Gupta <csai23031@glbitm.ac.in>
2026-01-10 22:58:26 +0530

    Time: 72 ms (7.59%) | Memory: 295.2 MB (36.68%) - LeetSync

---
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* dummy=new ListNode(0);
+        ListNode* tail=dummy;
+        int sum=0;
+        head=head->next;
+        while(head!=NULL){
+            if(head->val==0){
+                tail->next=new ListNode(sum);
+                tail=tail->next;
+                sum=0;
+            }
+            else sum+=head->val;
+            head=head->next;
+        }
+        return dummy->next;
+    }
+};
