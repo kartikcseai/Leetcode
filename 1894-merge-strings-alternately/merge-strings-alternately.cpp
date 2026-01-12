18fc6ac6a897a21c01d624cf04c033640cfdbbde
Karitk Gupta <csai23031@glbitm.ac.in>
2026-01-12 17:43:25 +0530

    Time: 4 ms (29.58%) | Memory: 8.5 MB (62.80%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result;
-        int i = 0, j = 0;
-        int n1 = word1.size(), n2 = word2.size();
-
-        // Merge characters alternately
-        while (i < n1 && j < n2) {
+        string result="";
+        int i=0,j=0;
+        while(word1.size()>i && word2.size()>j){
             result.push_back(word1[i++]);
             result.push_back(word2[j++]);
         }
-
-        // Append remaining characters from word1
-        while (i < n1) {
-            result.push_back(word1[i++]);
-        }
-
-        // Append remaining characters from word2
-        while (j < n2) {
-            result.push_back(word2[j++]);
-        }
-
+        while(i<word1.size()) result.push_back(word1[i++]);
+        while(j<word2.size()) result.push_back(word2[j++]);
         return result;
     }
 };
