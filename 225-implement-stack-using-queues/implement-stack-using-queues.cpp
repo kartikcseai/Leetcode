bfe83610f40103cd4bb967d20e9dfb6974a5ea94
Karitk Gupta <csai23031@glbitm.ac.in>
2026-01-27 22:51:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (57.01%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,20 @@
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {}
+    void push(int x) {
+        int n=q.size();
+        q.push(x);
+        for(int i=0;i<n;i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    int pop() {
+        int val=q.front();
+        q.pop();
+        return val;   
+    }
+    int top() { return q.front();}
+    bool empty() {return q.empty();}
+};
