60981d95c022f79d88a6c81d8b6658d434d6b7e7
Karitk Gupta <csai23031@glbitm.ac.in>
2026-02-05 23:16:04 +0530

    Time: 3 ms (28.39%) | Memory: 17.2 MB (31.81%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int ans = 0;
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2) {
+                    q.push({{i,j},0});
+                    visited[i][j] = true;
+                }
+            }
+        }
+        while(q.size() > 0){
+            int i = q.front().first.first;
+            int j = q.front().first.second;
+            int time  = q.front().second;
+            q.pop();
+            ans = max(ans, time);
+            if(i-1 >= 0 && !visited[i-1][j] && grid[i-1][j] == 1){
+                q.push({{i-1, j}, time + 1});
+                visited[i-1][j] = true;
+            }
+            if(j+1 < m && !visited[i][j+1] && grid[i][j+1] == 1){
+                q.push({{i, j+1}, time + 1});
+                visited[i][j+1] = true;
+            }
+            if(i+1 < n && !visited[i+1][j] && grid[i+1][j] == 1){
+                q.push({{i+1, j}, time + 1});
+                visited[i+1][j] = true;
+            }
+            if(j-1 >= 0 && !visited[i][j-1] && grid[i][j-1] == 1){
+                q.push({{i, j-1}, time + 1});
+                visited[i][j-1] = true;
+            }
+        }
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 1 && !visited[i][j]) return -1;
+            }
+        }
+        return ans;
+    }
+};
