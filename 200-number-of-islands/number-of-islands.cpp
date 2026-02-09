639444669b4d65760484c3bd87c8bc60a94d7922
Karitk Gupta <csai23031@glbitm.ac.in>
2026-02-09 19:07:13 +0530

    Time: 25 ms (71.55%) | Memory: 23.2 MB (30.45%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j){
-        if(i<0 || i>=grid.size() || j<0 || j>=grid[0].size() || grid[i][j]=='0') return;
-        grid[i][j]='0';
-        dfs(grid,i-1,j);
-        dfs(grid,i+1,j);
-        dfs(grid,i,j-1);
-        dfs(grid,i,j+1);
-    }
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()) return 0;
-        int is_land_count=0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(grid[i][j]=='1'){
-                    is_land_count++;
-                    dfs(grid,i,j);}
+        int rows = grid.size(), cols = grid[0].size();
+        int connected_components = 0;
+        for(int r = 0; r < rows; r++){
+            for(int c = 0; c < cols; c++){
+                if(grid[r][c] == '0') continue;
+                connected_components++;
+                grid[r][c] = '0';
+                queue<pair<int, int>> q;
+                q.push({r,c});
+                while(!q.empty()){
+                    auto curr = q.front(); q.pop();
+                    int curr_row = curr.first;
+                    int curr_col = curr.second;
+                    if(curr_row - 1 >= 0 && grid[curr_row - 1][curr_col] == '1'){
+                        q.push({curr_row - 1, curr_col});
+                        grid[curr_row - 1][curr_col] = '0';
+                    }
+                    if(curr_row + 1 < rows && grid[curr_row + 1][curr_col] == '1'){
+                        q.push({curr_row + 1, curr_col});
+                        grid[curr_row + 1][curr_col] = '0';
+                    }
+                    if(curr_col - 1 >= 0 && grid[curr_row][curr_col - 1] == '1'){
+                        q.push({curr_row, curr_col - 1});
+                        grid[curr_row][curr_col - 1] = '0';
+                    }
+                    if(curr_col + 1 < cols && grid[curr_row][curr_col + 1] == '1'){
+                        q.push({curr_row, curr_col + 1});
+                        grid[curr_row][curr_col + 1] = '0';
+                    }
+                }
             }
         }
-        return is_land_count;
+        return connected_components;
     }
 };
