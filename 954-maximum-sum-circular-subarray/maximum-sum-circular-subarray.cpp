3871a500beabc74ffb183bf9a9f5b32c05588c0f
Karitk Gupta <csai23031@glbitm.ac.in>
2026-02-18 12:01:06 +0530

    Time: 3 ms (20.09%) | Memory: 43.6 MB (48.78%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int kadane(const vector<int>& arr){
-        int max_sum=arr[0], curr_sum=arr[0];
-        for(int i=1;i<arr.size();i++){
-            curr_sum=max(arr[i],curr_sum+arr[i]);
-            max_sum=max(max_sum,curr_sum);
+    int max_sum_ka(vector<int>& nums){
+        int max_sum = nums[0], curr_sum = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            curr_sum = max(curr_sum + nums[i], nums[i]);
+            max_sum = max(max_sum, curr_sum);
         }
         return max_sum;
     }
+    int min_sum_ka(vector<int>& nums){
+        int min_sum = nums[0], curr_sum = nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            curr_sum = min(curr_sum + nums[i], nums[i]);
+            min_sum = min(min_sum, curr_sum);
+        }
+        return min_sum;
+    }
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n=nums.size();
-        int max_normal=kadane(nums);
-        vector<int> inverted_nums=nums;
-        for (int i=0;i<n;i++) inverted_nums[i]=-inverted_nums[i];
-        int min_normal=kadane(inverted_nums);
-        int total_sum=0;
-        for(int i=0;i<n;i++) total_sum+=nums[i];
-        int max_circular=total_sum+min_normal;
-        if(max_circular==0) return max_normal;
-        return max(max_normal,max_circular);
+        int total_sum = 0 ;
+        for(int i = 0; i < nums.size(); i++) total_sum += nums[i];
+        int max_sum = max_sum_ka(nums);
+        int min_sum = min_sum_ka(nums);
+        int circular_sum = total_sum - min_sum;
+        if (max_sum > 0) return max(max_sum, circular_sum);
+        return max_sum;
     }
 };
